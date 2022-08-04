a2c36603933134da8762589b85096204fb478808
Daiki Hatada <hatadaiki@gmail.com>
2022-08-05 00:26:37 +0900

    :sparkles: Add `fetchUserById`

---
+++ app/src/libs/users/fetchUserById.ts
@@ -0,0 +1,28 @@
+import { doc, getDoc } from "firebase/firestore";
+import { mockUsers } from "../../mocks/users";
+import { User } from "../../pages";
+import { db } from "../firebase";
+
+const fetchUserById = async (id: string): Promise<User> => {
+    // const snapshot = await getDoc(doc(db, "users", id));
+    // const user = {
+    //     id: snapshot.id,
+    //     ...snapshot.data(),
+    // }
+    const user = await fetchMockUserById(id)
+    return user
+}
+
+const fetchMockUserById = async (id: string): Promise<User> => {
+    await sleep(500)
+    return mockUsers.find(user => user.id === id)
+}
+
+const sleep = (msec: number): Promise<void> =>
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, msec)
+    })
+
+export default fetchUserById
